837ed7ad08cb5f319698408b90074dc25e909dcc
Görkem Tosun <gorkemtosun@Gorkem-MacBook-Pro.local>
2020-06-01 23:09:42 +0300

    Webpack configurations added

---
+++ webpack/webpack.common.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: ['babel-loader', 'eslint-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['*', '.js', '.jsx'],
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Product Showcase With GraphQL-Apollo',
+			template: './public/index.html',
+		}),
+	],
+	output: {
+		path: path.resolve(__dirname, '../', 'dist'),
+		publicPath: '/',
+		filename: 'bundle.js',
+	},
+};
